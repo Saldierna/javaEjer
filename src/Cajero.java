a9c27ab330c416f5b9ceb4048e2bc14f979c6bae
Luis Saldierna <luismario.saldierna.contractor@bbva.com>
2022-10-09 14:08:19 -0500

    cambio para hacer el merge

--- src/Cajero.java
+++ src/Cajero.java
@@ -10,7 +10,7 @@ public class Cajero {
     public static String menu = "¿Que deseas realizar?\n\n" +
             "    1.- Realizar un retiro.\n" +
             "    2.- Revisar saldo.\n" +
-            "    3.- Ultimo Movimiento CAMBIO DESDE DEVELOP A QA Y LUEGO A MASTER\n" +
+            "    3.- Ultimo Movimiento CAMBIO DESDE QA Y LUEGO A MASTER\n" +
             "    4.- Salirrrr.\n";
 
 
